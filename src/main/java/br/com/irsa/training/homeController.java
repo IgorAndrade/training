250e86f5c5bf3d11854b46b0167dcc024f158e0a
Igor Andrade <andrade.igor@gmail.com>
2014-08-06 23:19:37 -0300

    rest config

--- src/main/java/br/com/irsa/training/homeController.java
+++ src/main/java/br/com/irsa/training/homeController.java
@@ -3,18 +3,19 @@ package br.com.irsa.training;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/")
+import br.com.irsa.training.model.Usuario;
+
+@RestController
 public class homeController {
 
-		@RequestMapping("/home")
-		public String home(){
-			return "home";
+		@RequestMapping(value="/home",produces={"application/json"},method=RequestMethod.GET)
+		public Usuario home(){
+			Usuario usuario = new Usuario();
+			usuario.setNome("igor");
+			usuario.setEmail("fsdfsf");
+			return usuario;
 		}
 		
-		@RequestMapping(method=RequestMethod.GET)
-		public String index(){
-			return home();
-		}
 }
