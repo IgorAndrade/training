470f1408699f818552627e797a7d1c6d69e905f4
Igor Andrade <andrade.igor@gmail.com>
2014-08-24 19:44:37 -0300

    acertando o enum de status

--- src/main/java/br/com/irsa/training/model/StatusUser.java
+++ src/main/java/br/com/irsa/training/model/StatusUser.java
@@ -1,5 +1,19 @@
 package br.com.irsa.training.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum StatusUser {
-ATIVO,INATIVO,SUSPENSO
+ATIVO,INATIVO,SUSPENSO;
+
+@JsonValue
+public String getValue(){
+	return name();
+}
+
+@JsonCreator
+public static StatusUser create(String status) {
+
+	return valueOf(status);
+}
 }
