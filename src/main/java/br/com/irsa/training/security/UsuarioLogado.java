408269dd802784ae669f0026a08e3d81f9bbe10e
Igor Andrade <andrade.igor@gmail.com>
2014-08-26 23:59:09 -0300

    retirando usuario logado como bean de sessão

--- src/main/java/br/com/irsa/training/security/UsuarioLogado.java
+++ src/main/java/br/com/irsa/training/security/UsuarioLogado.java
@@ -4,64 +4,63 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
+import br.com.irsa.training.model.Permissoes;
 import br.com.irsa.training.model.StatusUser;
 import br.com.irsa.training.model.Usuario;
 
-@Component("userLogado")
-@Scope("session")
+//@Component("userLogado")
+//@Scope("session")
 public class UsuarioLogado implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Usuario user;
-	List<String> allPermissoes;
-	
+	List<Permissoes> allPermissoes;
+
 	public UsuarioLogado() {
-		this.allPermissoes = new ArrayList<String>();
+		this.allPermissoes = new ArrayList<Permissoes>();
+		this.user = null;
 	}
-	
+
 	public Usuario getUser() {
 		return user;
 	}
-	
+
 	public void setUser(Usuario user) {
 		this.user = user;
 	}
 
 	public String getNome() {
-		if(user==null)
+		if (user == null || user.getNome() == null)
 			return "Anonymous";
 		return user.getNome();
 	}
 
 	public String getEmail() {
-		if(user==null)
+		if (user == null)
 			return "Anonymous";
 		return user.getEmail();
 	}
 
 	public StatusUser getStatus() {
-		if(user==null)
+		if (user == null)
 			return StatusUser.INATIVO;
 		return user.getStatus();
 	}
-	
-	public void setAllPermissoes(List<String> allPermissoes) {
+
+	public void setAllPermissoes(List<Permissoes> allPermissoes) {
 		this.allPermissoes = allPermissoes;
 	}
-	
-	public List<String> getAllPermissoes() {
+
+	public List<Permissoes> getAllPermissoes() {
 		return allPermissoes;
 	}
-	
-	public boolean hasPermissao(String permissao){
+
+	public boolean hasPermissao(String permissao) {
 		return this.allPermissoes.contains(permissao);
 	}
-	
-	public void logOut(){
+
+	public void logOut() {
 		this.user = null;
 		this.allPermissoes.clear();
 	}
-	
+
 }
